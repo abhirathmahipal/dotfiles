6cd65fc510fa31b6ff96618c3b939d0544a794c1
Abhirath Mahipal <abhiabhiabhi27@gmail.com>
2026-04-20 16:30:31 +0530

    zsh updates.

--- setup-zshrc.sh
+++ setup-zshrc.sh
@@ -3,6 +3,11 @@ set -e
 
 DOTFILES="$(cd "$(dirname "$0")" && pwd)"
 
+if [ ! -d "$HOME/.oh-my-zsh" ]; then
+    echo "==> Installing Oh My Zsh..."
+    sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended
+fi
+
 echo "==> Copying .shell_common..."
 cp "$DOTFILES/.shell_common" "$HOME/.shell_common"
 
